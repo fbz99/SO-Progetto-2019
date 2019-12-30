34fd1a16362a318407a077d5601841d8753b4620
RobertoBombieri <58972897+RobertoBombieri@users.noreply.github.com>
2019-12-30 11:36:48 +0100

    Sistemato file bandierina

--- project/bandierina.c
+++ project/bandierina.c
@@ -10,8 +10,8 @@
 #define SO_NUM_G 2
 #define SO_NUM_P 10
 #define SO_MAX_TIME 3
-#define SO_BASE 60
-#define SO_ALTEZZA 20
+#define SO_BASE 6
+#define SO_ALTEZZA 2
 #define SO_FLAG_MIN 5
 #define SO_FLAG_MAX 5
 #define SO_ROUND_SCORE 10
@@ -21,32 +21,37 @@ int casuale();
 int main(){
     
     int tot = SO_ROUND_SCORE;
-    int max_rand;                                           
+    int max_rand, size = SO_ALTEZZA*SO_BASE ;
+    char matrice[SO_ALTEZZA*SO_BASE];
+    char *mtr;                                          
     int flag = (rand() % (SO_FLAG_MAX - SO_FLAG_MIN + 1)) + SO_FLAG_MIN;
-    int band;
-    int i, j=0;
-    int tmp= flag;
+    int band, i, j, pos, rand_pos;
+    int tmp = flag;
     key_t key = 12345;
-    int shm_id = shmget (key, sizeof(int)*(SO_BASE*SO_ALTEZZA),0600);
-    char *matrix;
-    matrix = shmat(shm_id,  NULL, 0);
+    int mat_id = shmget (key, sizeof(int)*(SO_BASE*SO_ALTEZZA),0600);
+    mtr = &matrice[0]; 
+    mtr = shmat(mat_id, NULL, 0);
     
+    for(mtr=&matrice[0];*mtr;mtr++){
+        *mtr='0';
+    }
+
     /*printf("%d\n",flag);*/
     srand(time(NULL));
     for(i=0;i<flag;i++){
+        rand_pos = casuale(size, 0);
     	max_rand = tot-(tmp-1);
     	if(tmp != 1)
         band = casuale(max_rand,1);
     	else band = tot;
-        tot= tot - band;
-        /*band = *matrix + casuale(0,SO_BASE*SO_ALTEZZA) ;
-       for (j;j<SO_BASE*SO_ALTEZZA;j++){
-            printf("posizione %d - valore %c", *matrix, &matrix+j);         /*TODO -- Stampa dell'array in memoria condivisa per poter visualizzare le bandierine*/
-        /*}*/
-    	printf("TOT:%d -- FLAG:%d -- BAND: %d\n",tot,tmp,band);
+        tot = tot - band;
+        matrice[rand_pos] = band;
+        printf("TOT:%d -- FLAG:%d -- BAND: %d\n",tot,tmp,band);
     	tmp--;
     }
-
+for (j=0;j<size;j++){
+            printf("posizione %d - valore %c - rand_pos %d\n", j, matrice[rand_pos], rand_pos);         
+        }
     while (wait(NULL) != -1);
 }
 
--- project/main.c
+++ project/main.c
@@ -9,25 +9,33 @@
 #define SO_NUM_G 2
 #define SO_NUM_P 10
 #define SO_MAX_TIME 3
-#define SO_BASE 60
-#define SO_ALTEZZA 20
+#define SO_BASE 5
+#define SO_ALTEZZA 2
 #define SO_FLAG_MIN 5
 #define SO_FLAG_MAX 5
 #define SO_ROUND_SCORE 10
 #define SO_N_MOVES 20
 #define SO_MIN_HOLD_NSEC 10000000
 
+int casuale();
 int main(){
-    int i;
+    int i, size = SO_ALTEZZA*SO_BASE;
     pid_t players[SO_NUM_G];
     int *ptr = players;
-    char *matrix;
+    int *matrice;
     int flag_n = (rand() % (SO_FLAG_MAX - SO_FLAG_MIN + 1)) + SO_FLAG_MIN; 
     key_t key = 12345;
+    int tot = SO_ROUND_SCORE;
+    int max_rand;                                           
+    int flag = (rand() % (SO_FLAG_MAX - SO_FLAG_MIN + 1)) + SO_FLAG_MIN;
+    int band, j=0, rand_pos, pos;
+    int tmp = flag;
+    int mat_id = shmget (key, (sizeof(int)*(SO_BASE)*(SO_ALTEZZA)), IPC_CREAT | 0666);
+    matrice = shmat(mat_id, NULL, 0);
 
-    int shm_id = shmget (key, sizeof(int)*(SO_BASE*SO_ALTEZZA),0600);
-    matrix = shmat(shm_id, NULL, 0);
 
+       	for (pos = 0; pos < size; pos++) /*SETTAGGIO MATRICE*/
+		matrice[pos] = 0;
     printf("Master process: %d\n",getpid());
 
 
@@ -50,7 +58,32 @@ int main(){
        
     }
 
-    execve("./bandierina",NULL,NULL);	/*Riporta al file in cui vengono create le bandierine*/
+        /*
+        CREAZIONE E POSIZIONAMENTO DELLE BANDIERINE
+        printf("%d\n",flag);*/
+    /*srand(time(NULL));
+    for(i=0;i<flag;i++){
+
+        rand_pos = casuale(0, size);
+    	max_rand = tot-(tmp-1);
+    	if(tmp != 1)
+        band = casuale(max_rand,1);
+    	else band = tot;
+        tot = tot - band;
+        matrice[rand_pos] = band;
+        for (j;j<SO_BASE*SO_ALTEZZA;j++){
+            printf("posizione %d - valore %c - pid = %d - rand_pos %d\n", j, matrice[rand_pos],getpid(), rand_pos);         
+       }
+    	printf("TOT:%d -- FLAG:%d -- BAND: %d\n",tot,tmp,band);
+    	tmp--;
+    }*/
+    execve("./bandierina",NULL,NULL);
+
 
     while (wait(NULL) != -1);
 }
+
+int casuale(int a,int b)  
+{  
+    return rand() % (b - a + 1) + a;
+}  
