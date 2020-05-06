d26ef11e638014d54d15d1f50ea6ee3351c54b1e
RobertoBombieri <roberto.bombieri@edu.unito.it>
2020-05-06 17:00:43 +0200

    Player - Struct msgbuf

--- project/player.c
+++ project/player.c
@@ -1,8 +1,7 @@
 #include "Libreria.h"
 
 int main(int argc,const char *args[]){
-	/*printf("PID1: %d\n",getpid());*/
-    int size = SO_ALTEZZA*SO_BASE;
+	printf("PID1: %d",getpid());
     struct stato_player giocatore;  
     struct msgbuf my_msg;
     int i,rand_pos;
@@ -19,8 +18,6 @@ int main(int argc,const char *args[]){
     int mat_id = shmget (key, sizeof(int)*(SO_BASE*SO_ALTEZZA), IPC_CREAT |0666);
     matrice = shmat(mat_id, NULL, 0);
     
-    sem_id_mat= semget (key2, size, IPC_CREAT | 0666); /*Collegamento array semafori matrice*/
-
     /*Definizione stato (Struct)*/
     ptr = malloc(sizeof(int)*SO_NUM_P);
     giocatore.pid=getpid();
@@ -33,40 +30,15 @@ int main(int argc,const char *args[]){
     reserveSem(sem_id_mutex,0);
 
     /*Inserimento pedine in posizione casuale*/
-    for (pos = 0; pos < SO_NUM_P; pos++) /*Azzero old pos*/
+    for (pos = 0; pos < SO_NUM_P; pos++) /*SETTAGGIO old pos*/
 		old_pos[pos] = 0;
 	
-    srand(getpid());
-   /* for(i=0;i<SO_NUM_P;i++){
-       /* rand_pos = casuale(SO_BASE * SO_ALTEZZA, 0);*/
-       /* while(semctl(sem_id_mat,rand_pos,GETVAL)!=1){
-            rand_pos = casuale(SO_BASE * SO_ALTEZZA, 0);
-        }
-        printf("Giocatore: %c\n",giocatore.giocatore);
-        old_pos[i]=rand_pos;
-        matrice[rand_pos] = giocatore.giocatore;
-        switch(ptr[i] = fork()){
-        case -1:
-            printf("error\n");
-            exit(0);
-            break;
-
-        case 0: /*Processo figlio*/
-            /*itoa(old_pos[i],stringa);
-          	copiaArray(my_msg.mtext,stringa);
-            msgsnd(msg_id, &my_msg, MSG_LEN, 0);  /*Invio del messaggio nella coda*/
-           /* execve("./pedina", args1, NULL);
-            exit(1);
-            break;
-        } */
+    srand(getpid());	/*Aggiunto*/
     for(i=0;i<SO_NUM_P;i++){
-       /* rand_pos = casuale(SO_BASE * SO_ALTEZZA, 0);*/
-        while(semctl(sem_id_mat,rand_pos,GETVAL)!=1){
-            rand_pos = casuale(SO_BASE * SO_ALTEZZA, 0);
-        }
+        rand_pos = casuale(SO_BASE * SO_ALTEZZA, 0);
         printf("Giocatore: %c\n",giocatore.giocatore);
         old_pos[i]=rand_pos;
-        
+        if(val_check(rand_pos, old_pos) == 1)rand_pos = casuale(SO_BASE * SO_ALTEZZA,0);
         matrice[rand_pos] = giocatore.giocatore;
     }
 
@@ -79,7 +51,7 @@ int main(int argc,const char *args[]){
             break;
 
         case 0: /*Processo figlio*/
-            /*itoa(old_pos[i],stringa);
+            itoa(old_pos[i],stringa);
           	copiaArray(my_msg.mtext,stringa);
             msgsnd(msg_id, &my_msg, MSG_LEN, 0);  /*Invio del messaggio nella coda*/
             execve("./pedina", args1, NULL);
@@ -88,7 +60,6 @@ int main(int argc,const char *args[]){
         } 
     }
 
-   /* args1[1] = NULL;*/
     releaseSem(sem_id_mutex,0);
     while (wait(NULL) != -1);
 }
