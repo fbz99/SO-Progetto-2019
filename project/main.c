16707955834c9ba96aac80a87d005c663c57635f
robertobombieri <roberto.bombieri@edu.unito.it>
2020-05-09 23:08:15 +0200

    Problema: non stampa le pedina sulla scacchiera

--- project/main.c
+++ project/main.c
@@ -1,94 +1,95 @@
 #include "Libreria.h"
 
-
-int main(){
-    int i, size = SO_ALTEZZA*SO_BASE;
+int main()
+{
+    int i, size = SO_ALTEZZA * SO_BASE;
     int *ptr;
-    int flag_n = (rand() % (SO_FLAG_MAX - SO_FLAG_MIN + 1)) + SO_FLAG_MIN; 
+    int flag_n = (rand() % (SO_FLAG_MAX - SO_FLAG_MIN + 1)) + SO_FLAG_MIN;
     int tot = SO_ROUND_SCORE;
-    int max_rand;                                           
+    int max_rand;
     int flag = (rand() % (SO_FLAG_MAX - SO_FLAG_MIN + 1)) + SO_FLAG_MIN;
-    int band, j=0, rand_pos, pos;
+    int band, j = 0, rand_pos, pos;
     int tmp = flag;
-    int status; 
+    int status;
     int *old_pos;
     pid_t wpid;
-    char * args [2]; 
-    char *matrice;     
-    char stringa[4];  
-    int sem_id_mat, sem_id_mutex, sem_id_zero;  
+    char *args[2];
+    char *matrice;
+    char stringa[4];
+    int sem_id_mat, sem_id_mutex, sem_id_zero;
+    int mat_id = shmget(key, sizeof(int) * (SO_BASE * SO_ALTEZZA), IPC_CREAT | 0666);
 
-    int mat_id = shmget (key, sizeof(int)*(SO_BASE*SO_ALTEZZA), IPC_CREAT |0666);
     matrice = shmat(mat_id, NULL, 0);
-   	/*printf("%d", mat_id);*/
+    /*printf("%d", mat_id);*/
 
-       	for (pos = 0; pos <= size; pos++) /*SETTAGGIO MATRICE*/
-		matrice[pos] = '0';
+    for (pos = 0; pos <= size; pos++) /*SETTAGGIO MATRICE*/
+        matrice[pos] = '0';
 
-		/*Creazione semafori sulla matrice*/
-		sem_id_mat= semget (key2, size, IPC_CREAT | 0666);
-		for(i=0;i<size;i++){
-			initSemAvaiable(sem_id_mat,i);
-		}
+    /*Creazione semafori sulla matrice*/
+    sem_id_mat = semget(key2, size, IPC_CREAT | 0666);
+    for (i = 0; i < size; i++)
+    {
+        initSemAvaiable(sem_id_mat, i);
+    }
 
-		/*Creazione array di semafori per attesa dei giocatori (tutti a 0 tranne il primo)*/
-		sem_id_mutex = semget (key3, 1, IPC_CREAT | 0666);
-		initSemAvaiable(sem_id_mutex,0);
+    /*Creazione array di semafori per attesa dei giocatori (tutti a 0 tranne il primo)*/
+    sem_id_mutex = semget(key3, 1, IPC_CREAT | 0666);
+    initSemAvaiable(sem_id_mutex, 0);
 
-		/*sem_id_zero = semget(key0,4, IPC_CREAT | 0666);*/
-    args[1]=NULL;
-    ptr = malloc(sizeof(int)*SO_NUM_G);
-    
+    /*sem_id_zero = semget(key0,4, IPC_CREAT | 0666);*/
+    args[1] = NULL;
+    ptr = malloc(sizeof(int) * SO_NUM_G);
+
     /*Creazione Giocatori*/
-    for(i=0 ;i<SO_NUM_G;i++){ 
-       switch(ptr[i] = fork()){
-            case -1: 
-                printf("Error\n");
-                exit(0);
-                break;
-            case 0: /*Processo figlio*/
-                sprintf(stringa, "%d", i+1);
-                args[0]= stringa;
-                execve("./player", args, args[1]);
-                printf("PID Giocatore : %d\n", getpid());
-                break;
+    for (i = 0; i < SO_NUM_G; i++)
+    {
+        switch (ptr[i] = fork())
+        {
+        case -1:
+            printf("Error\n");
+            exit(0);
+            break;
+        case 0: /*Processo figlio*/
+            sprintf(stringa, "%d", i + 1);
+            args[0] = stringa;
+            execve("./player", args, NULL);
+            printf("PID Giocatore : %d\n", getpid());
+            break;
 
             /*default: 
                 printf("default fork\n");
                 break;*/
-       } 
-       
+        }
     }
     /*aspetta_zero(sem_id_zero,0);*/
     /*while ((wpid = wait(&status)) > 0){ */
 
-    
-    old_pos = malloc(sizeof(int)*flag);
+    old_pos = malloc(sizeof(int) * flag);
     srand(time(NULL));
-    for(i=0;i<flag;i++){
-        
+    for (i = 0; i < flag; i++)
+    {
+
         rand_pos = casuale(size, 0);
-        old_pos[i]=rand_pos;
-        if((val_check(rand_pos, old_pos) == 1)&&pos_check(rand_pos,matrice)==1) rand_pos = casuale(size,0);
-        
-    	max_rand = tot-(tmp-1);
-    	if(tmp != 1){
-            band = casuale(max_rand,1);
+        old_pos[i] = rand_pos;
+        if ((val_check(rand_pos, old_pos) == 1) && pos_check(rand_pos, matrice) == 1)
+            rand_pos = casuale(size, 0);
+
+        max_rand = tot - (tmp - 1);
+        if (tmp != 1)
+        {
+            band = casuale(max_rand, 1);
         }
-    	else band = tot;
+        else
+            band = tot;
         tot = tot - band;
-        
-        if(band!=0){
-        	reserveSem(sem_id_mat,rand_pos+1);
-        	matrice[rand_pos] = band+48;
+
+        if (band != 0)
+        {
+            reserveSem(sem_id_mat, rand_pos + 1);
+            matrice[rand_pos] = band + 48;
         }
-    	tmp--;
-        
+        tmp--;
     }
     stampa_scacchiera();
-    /*shmctl(mat_id, IPC_RMID, NULL);  /*RIMOZIONE MEMORIA CONDIVISA*/
     while (wait(NULL) != -1);
 }
-
-
-
--- project/player.c
+++ project/player.c
@@ -1,50 +1,60 @@
 #include "Libreria.h"
 
-int main(int argc,const char *args[]){
-	printf("PID1: %d",getpid());
-    struct stato_player giocatore;  
+int main(int argc, const char *args[])
+{
+    /*printf("PID1: %d\n", getpid());*/
+    struct stato_player giocatore;
     struct msgbuf my_msg;
-    int i,rand_pos;
+    int i, rand_pos, j = 0;
     int *ptr, pos;
     int *old_pos;
     char *args1[2];
-    char *matrice; 
+    char *matrice;
     char stringa[10];
-    int msg_id;   
+    int msg_id;
     int sem_id_mat, sem_id_mutex, sem_id_zero;
-    
+
     /*char *my_messaggio;
     printf(" sizeof char %ld", sizeof(my_messaggio));*/
-    int mat_id = shmget (key, sizeof(int)*(SO_BASE*SO_ALTEZZA), IPC_CREAT |0666);
+    int mat_id = shmget(key, sizeof(int) * (SO_BASE * SO_ALTEZZA), IPC_CREAT | 0666);
     matrice = shmat(mat_id, NULL, 0);
-    
+
     /*Definizione stato (Struct)*/
-    ptr = malloc(sizeof(int)*SO_NUM_P);
-    giocatore.pid=getpid();
-    giocatore.giocatore =  (atoi(args[0]))+64;
+    ptr = malloc(sizeof(int) * SO_NUM_P);
+    giocatore.pid = getpid();
+    giocatore.giocatore = (atoi(args[0])) + 64;
 
     /*Creazione coda di messaggi*/
     msg_id = msgget(key_coda, IPC_CREAT | 0666);
-    
+
     sem_id_mutex = semget(key3, 1, IPC_CREAT | 0666);
-    reserveSem(sem_id_mutex,0);
+    reserveSem(sem_id_mutex, 0);
 
     /*Inserimento pedine in posizione casuale*/
     for (pos = 0; pos < SO_NUM_P; pos++) /*SETTAGGIO old pos*/
-		old_pos[pos] = 0;
-	
-    srand(getpid());	/*Aggiunto*/
+        old_pos[pos] = 0;
 
-    for(i=0;i<SO_NUM_P;i++){
-       /* rand_pos = casuale(SO_BASE * SO_ALTEZZA, 0);*/
-        while(semctl(sem_id_mat,rand_pos,GETVAL)!=1){
+    srand(getpid()); /*Aggiunto*/
+
+    args1[1] = NULL;
+    for (i = 0; i < SO_NUM_P; i++)
+    {
+        rand_pos = casuale(SO_BASE * SO_ALTEZZA, 0);
+        while (semctl(sem_id_mat, rand_pos, GETVAL) != 1)
+        {
+            /*printf("%d\n",semctl(sem_id_mat,rand_pos,GETVAL));*/
             rand_pos = casuale(SO_BASE * SO_ALTEZZA, 0);
         }
-        printf("Giocatore: %c\n",giocatore.giocatore);
-        old_pos[i]=rand_pos;
-        
+        old_pos[i] = rand_pos;
+
+        sprintf(stringa, "%d", rand_pos);
+        args1[0] = stringa;
         matrice[rand_pos] = giocatore.giocatore;
-        switch(ptr[i] = fork()){
+
+        printf("Giocatore: %c\n", giocatore.giocatore);
+
+        switch (ptr[i] = fork())
+        {
         case -1:
             printf("error\n");
             exit(0);
@@ -59,6 +69,8 @@ int main(int argc,const char *args[]){
             break;
         }
     }
-    releaseSem(sem_id_mutex,0);
-    while (wait(NULL) != -1);
+    matrice[0] = 65;
+    releaseSem(sem_id_mutex, 0);
+    while (wait(NULL) != -1)
+        ;
 }
